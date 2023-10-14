e98a3cb1e4daf05ccbab5d76f8735acb096d2a87
DBAPTZ <123421505+DBAPTZ@users.noreply.github.com>
2023-10-14 16:31:46 -0400

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -1,39 +1,115 @@
-// Function to generate a random equipment
-function generateRandomEquipment() {
-    const equipmentTypes = ["Lawnmower", "Drill", "Saw", "Weedwacker", "Wrench"];
-    const randomEquipment = equipmentTypes[Math.floor(Math.random() * equipmentTypes.length)];
-    const randomName = `Brand${Math.floor(Math.random() * 10) + 1} ${randomEquipment}`;
-    const description = `This is a ${randomEquipment} from Brand${Math.floor(Math.random() * 10) + 1}.`;
-    const price = (Math.random() * 1000).toFixed(2); // Random price between 0 and 1000
-    const statuses = ["In stock", "Out of stock", "Delivering"];
-    const randomStatus = statuses[Math.floor(Math.random() * statuses.length)];
-    const amount = Math.floor(Math.random() * 10) + 1; // Random amount between 1 and 10\
-    const colour = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Pink", "White", "Grey", "Black"];
-    const randomColour = colour[Math.floor(Math.random() * colour.length)];
+// Define an array of hard-coded equipment
+const hardCodedEquipment = [
+    {
+        equipment: "Lawnmower",
+        name: "Brand1 Lawnmower",
+        description: "This is a Lawnmower from Brand1.",
+        price: "250.00",
+        status: "In stock",
+        amount: 5,
+        colour: "Green"
+    },
+    {
+        equipment: "Drill",
+        name: "Brand2 Drill",
+        description: "This is a Drill from Brand2.",
+        price: "120.00",
+        status: "Out of stock",
+        amount: 0,
+        colour: "Red"
+    },
+    {
+        equipment: "Saw",
+        name: "Brand3 Saw",
+        description: "This is a Saw from Brand3.",
+        price: "180.00",
+        status: "In stock",
+        amount: 8,
+        colour: "Blue"
+    },
+    {
+        equipment: "Weedwacker",
+        name: "Brand4 Weedwacker",
+        description: "This is a Weedwacker from Brand4.",
+        price: "150.00",
+        status: "Delivering",
+        amount: 2,
+        colour: "Yellow"
+    },
+    {
+        equipment: "Wrench",
+        name: "Brand5 Wrench",
+        description: "This is a Wrench from Brand5.",
+        price: "90.00",
+        status: "In stock",
+        amount: 4,
+        colour: "Orange"
+    },
+    {
+        equipment: "Chainsaw",
+        name: "Brand6 Chainsaw",
+        description: "This is a Chainsaw from Brand6.",
+        price: "280.00",
+        status: "In stock",
+        amount: 6,
+        colour: "Black"
+    },
+    {
+        equipment: "Hammer",
+        name: "Brand7 Hammer",
+        description: "This is a Hammer from Brand7.",
+        price: "15.00",
+        status: "In stock",
+        amount: 10,
+        colour: "Grey"
+    },
+    {
+        equipment: "Screwdriver",
+        name: "Brand8 Screwdriver",
+        description: "This is a Screwdriver from Brand8.",
+        price: "10.00",
+        status: "Out of stock",
+        amount: 0,
+        colour: "Red"
+    },
+    {
+        equipment: "Shovel",
+        name: "Brand9 Shovel",
+        description: "This is a Shovel from Brand9.",
+        price: "30.00",
+        status: "In stock",
+        amount: 3,
+        colour: "Green"
+    },
+    {
+        equipment: "Rake",
+        name: "Brand10 Rake",
+        description: "This is a Rake from Brand10.",
+        price: "25.00",
+        status: "In stock",
+        amount: 7,
+        colour: "Yellow"
+    }
+];
 
-    return {
-        equipment: randomEquipment,
-        name: randomName,
-        description: description,
-        price: price,
-        status: randomStatus,
-        amount: amount,
-        colour: randomColour
-    };
-}
-
-// Function to fill the equipment table with randomly generated equipment
-function fillTableWithRandomEquipment() {
+// Function to fill the equipment table with hard-coded equipment and an Edit button
+function fillTableWithHardCodedEquipment() {
     const table = document.getElementById("equipmentTable");
     const tbody = table.getElementsByTagName("tbody")[0];
 
-    for (let i = 0; i < 10; i++) { // Generate 10 random equipment
-        const equipment = generateRandomEquipment();
+    for (let i = 0; i < hardCodedEquipment.length; i++) {
+        const equipment = hardCodedEquipment[i];
         const row = document.createElement("tr");
-       row.innerHTML = `<td>${equipment.equipment}</td>
-        <td>${equipment.name}</td><td>${equipment.description}</td>
-        <td>$${equipment.price}</td><td>${equipment.status}</td>
-        <td>${equipment.amount}</td><td>${equipment.colour}</td>`;
+        row.innerHTML = `
+            <td>${equipment.equipment}</td>
+            <td>${equipment.name}</td>
+            <td>${equipment.description}</td>
+            <td>$${equipment.price}</td>
+            <td>${equipment.status}</td>
+            <td>${equipment.amount}</td>
+            <td>${equipment.colour}</td>
+            <td><button onclick="editEquipment(${i})">Edit</button></td>
+        `;
         tbody.appendChild(row);
     }
 }
@@ -69,8 +145,17 @@ function filterEquipment() {
     }
 }
 
-//Fill table with randomly generated equipment
-document.addEventListener("DOMContentLoaded", function() {
+// Function to handle editing equipment
+function editEquipment(index) {
+    // You can access the equipment data in the hardCodedEquipment array using the index
+    const equipmentToEdit = hardCodedEquipment[index];
+
+    // Redirect the user to the edit.html page with the equipment's index as a query parameter
+    window.location.href = `editinventory.html?index=${index}`;
+}
+
+// Fill table with hard-coded equipment and "Edit" buttons
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("searchInput").addEventListener("input", filterEquipment);
-    fillTableWithRandomEquipment();
+    fillTableWithHardCodedEquipment(); // Call the modified function
 });
