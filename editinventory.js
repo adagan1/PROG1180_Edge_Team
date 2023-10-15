50e1ca0903edf3cf5874c57a4c86f87ab1146eb7
DBAPTZ <123421505+DBAPTZ@users.noreply.github.com>
2023-10-14 20:16:43 -0400

    editinventory.js

--- editinventory.js
+++ editinventory.js
@@ -1,56 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the index from the URL query parameter
+    // Get the equipment data from the URL parameter as a JSON string
     const urlParams = new URLSearchParams(window.location.search);
-    const index = parseInt(urlParams.get("index"));
+    const equipmentDataString = urlParams.get("equipmentData");
 
-    // Get the equipment data from the inventory page (inventory.html)
-    const equipmentData = getEquipmentDataFromInventoryPage(index);
+    if (equipmentDataString) {
+        try {
+            // Parse the JSON data
+            const equipmentToEdit = JSON.parse(equipmentDataString);
 
-    // Populate the form fields with the equipment data
-    document.getElementById("equipment").value = equipmentData.equipment;
-    document.getElementById("name").value = equipmentData.name;
-    document.getElementById("description").value = equipmentData.description;
-    document.getElementById("price").value = equipmentData.price;
-    document.getElementById("status").value = equipmentData.status;
-    document.getElementById("amount").value = equipmentData.amount;
-    document.getElementById("colour").value = equipmentData.colour;
+            // Populate the input fields with the equipment data
+            document.getElementById("equipmentName").value = equipmentToEdit.name;
+            document.getElementById("equipmentDescription").value = equipmentToEdit.description;
+            document.getElementById("equipmentPrice").value = equipmentToEdit.price;
+            document.getElementById("equipmentStatus").value = equipmentToEdit.status;
+            document.getElementById("equipmentAmount").value = equipmentToEdit.amount;
+            document.getElementById("equipmentColour").value = equipmentToEdit.colour;
 
-    // Enable the form fields for editing
-    const saveButton = document.getElementById("saveButton");
-    saveButton.addEventListener("click", function () {
-        // Update the equipment data with the edited values
-        equipmentData.equipment = document.getElementById("equipment").value;
-        equipmentData.name = document.getElementById("name").value;
-        equipmentData.description = document.getElementById("description").value;
-        equipmentData.price = document.getElementById("price").value;
-        equipmentData.status = document.getElementById("status").value;
-        equipmentData.amount = document.getElementById("amount").value;
-        equipmentData.colour = document.getElementById("colour").value;
+            // Add an event listener to save changes
+            document.getElementById("saveButton").addEventListener("click", function () {
+                // Update the equipment data with the new values
+                equipmentToEdit.name = document.getElementById("equipmentName").value;
+                equipmentToEdit.description = document.getElementById("equipmentDescription").value;
+                equipmentToEdit.price = document.getElementById("equipmentPrice").value;
+                equipmentToEdit.status = document.getElementById("equipmentStatus").value;
+                equipmentToEdit.amount = document.getElementById("equipmentAmount").value;
+                equipmentToEdit.colour = document.getElementById("equipmentColour").value;
 
-        // Save the edited data to local storage
-        saveEquipmentDataToLocalStorage(index, equipmentData);
+                // Update the data in local storage
+                // Retrieve the existing equipment data
+                const storedEquipmentData = JSON.parse(localStorage.getItem("equipmentData")) || [];
 
-        // Redirect back to the inventory page
-        window.location.href = "inventory.html";
-    });
-});
+                // Update the equipment data in local storage
+                storedEquipmentData[equipmentToEdit.index] = equipmentToEdit;
 
-// Function to get equipment data from the inventory page based on the index
-function getEquipmentDataFromInventoryPage(index) {
-    // Assuming that the inventory page (inventory.html) defines the equipment data in a global variable called "hardCodedEquipment"
-    // You can access the equipment data using the index
-    if (Array.isArray(hardCodedEquipment) && index >= 0 && index < hardCodedEquipment.length) {
-        return hardCodedEquipment[index];
+                // Save the updated data back to local storage
+                localStorage.setItem("equipmentData", JSON.stringify(storedEquipmentData));
+
+                // Redirect back to the inventory page
+                window.location.href = "inventory.html";
+            });
+        } catch (error) {
+            console.error("Error parsing equipment data:", error);
+        }
     } else {
-        // Handle the case where the index is out of bounds or "hardCodedEquipment" is not available
-        console.error("Equipment data not found.");
-        return null; // or return a default value or error handling logic
+        // Handle missing or invalid equipment data
+        alert("Invalid or missing equipment data.");
+        window.location.href = "inventory.html";
     }
-}
-
-// Function to save edited equipment data to local storage
-function saveEquipmentDataToLocalStorage(index, equipmentData) {
-    const updatedEquipmentData = JSON.parse(localStorage.getItem('equipmentData')) || [];
-    updatedEquipmentData[index] = equipmentData;
-    localStorage.setItem('equipmentData', JSON.stringify(updatedEquipmentData));
-}
+});
