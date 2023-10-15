16a724e8b758f8e365e9a57ff3be5f8a1769e5d8
Kzizian <145236803+Kzizian@users.noreply.github.com>
2023-10-15 15:25:53 -0400

    Create customerCreate.js

---
+++ customerCreate.js
@@ -0,0 +1,26 @@
+function createInventory() {
+    // Get the data for the new inventory item
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const address = document.getElementById("address").value;
+    const city = document.getElementById("city").value;
+
+    // Create an object with the new equipment data
+    const newCustomer = {
+        name: name,
+        age: age,
+        email: email,
+        phone: phone,
+        address: address,
+        city: city
+    };
+
+    // You can save the new data to local storage, a database, or perform any necessary actions here
+    // For this example, we'll log the new data to the console
+    console.log("New Equipment Data:", newCustomer);
+
+    // Redirect back to the inventory page
+    window.location.href = "customer.html";
+}
