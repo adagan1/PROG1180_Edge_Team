e85c7764b99aa6bf9dd8a9dc5ffddc5c8ee91ab9
DBAPTZ <123421505+DBAPTZ@users.noreply.github.com>
2023-10-27 15:57:25 -0400

    Update customerCreate.js

--- customerCreate.js
+++ customerCreate.js
@@ -1,25 +1,31 @@
-function createInventory() {
+function createCustomer() {
     // Get the data for the new inventory item
     const name = document.getElementById("name").value;
-    const age = document.getElementById("age").value;
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
     const address = document.getElementById("address").value;
     const city = document.getElementById("city").value;
+    const postal = document.getElementById("postal").value;
+    const province = document.getElementById("province").value;
 
     // Create an object with the new equipment data
     const newCustomer = {
         name: name,
-        age: age,
         email: email,
         phone: phone,
         address: address,
-        city: city
+        city: city,
+        postal: postal,
+        province: province
     };
 
     // For this example, we'll log the new data to the console
     console.log("New Equipment Data:", newCustomer);
 
     // Redirect back to the inventory page
+    window.location.href = "customer.html";
+}
+
+function goToCustomerPage() {
     window.location.href = "customer.html";
 }
