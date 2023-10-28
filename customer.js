b51fab69d493011db9dd9ca795dbc771654db599
DBAPTZ <123421505+DBAPTZ@users.noreply.github.com>
2023-10-28 11:15:44 -0400

    Update customer.js

--- customer.js
+++ customer.js
@@ -1,10 +1,10 @@
-// Define an array of hard-coded equipment
+// Define an array of hard-coded customer data
 const hardCodedCustomer = [
     {
         name: "Joe Jawndel",
         email: "JJawndel@gmail.com",
         phone: "905-221-3490",
-        address: "1220 Royal lane",
+        address: "1220 Royal Lane",
         city: "Niagara Falls",
         postal: "A2V 3NM",
         province: "Ontario"
@@ -29,8 +29,8 @@ const hardCodedCustomer = [
     }
 ];
 
-// Function to fill the customer table with hard-coded customers and an Edit button
-function fillTableWithHardCodedEquipment() {
+// Function to fill the table with customer data, including "Edit" and "Detail" buttons
+function fillTableWithCustomerData() {
     const table = document.getElementById("customerTable");
     const tbody = table.getElementsByTagName("tbody")[0];
 
@@ -41,109 +41,42 @@ function fillTableWithHardCodedEquipment() {
             <td>${customer.name}</td>
             <td>${customer.email}</td>
             <td>${customer.phone}</td>
-            <td>${customer.address}</td>
-            <td>${customer.city}</td>
-            <td>${customer.postal}</td>
-            <td>${customer.province}</td>
             <td>
-            <button onclick="editCustomer(${i})">Edit</button>&nbsp;
-            <button onclick="detailCustomer(${i})">Detail</button>
+                <button onclick="editCustomer(${i})">Edit</button>
+                <button onclick="detailCustomer(${i})">Detail</button>
             </td>
         `;
         tbody.appendChild(row);
     }
 }
 
-// Function to initialize local storage with sample data
-function initializeLocalStorageWithData() {
-    // Store the sample data in local storage
-    localStorage.setItem("customerData", JSON.stringify(hardCodedCustomer));
+// Function to display customer details when the "Detail" button is clicked
+function detailCustomer(index) {
+    // Get the customer data to display
+    const customerData = hardCodedCustomer[index];
+
+    // Convert the customer data to a JSON string
+    const customerDataString = JSON.stringify(customerData);
+
+    // Store the selected customer data in sessionStorage for access on the new page
+    sessionStorage.setItem("selectedCustomer", customerDataString);
+
+    // Redirect to the customerDetail.html page
+    window.location.href = "customerDetail.html";
 }
 
-// Function to fill the equipment table with data from local storage
-function fillTableWithEquipmentFromLocalStorage() {
-    // Retrieve the equipment data from local storage
-    const storedCustomerData = JSON.parse(localStorage.getItem("customerData")) || [];
-
-    const table = document.getElementById("customerTable");
-    const tbody = table.getElementsByTagName("tbody")[0];
-
-    // Clear existing rows
-    tbody.innerHTML = "";
-
-    for (let i = 0; i < storedCustomerData.length; i++) {
-        const customer = storedCustomerData[i];
-        const row = document.createElement("tr");
-        row.innerHTML = `
-        <td>${customer.name}</td>
-        <td>${customer.email}</td>
-        <td>${customer.phone}</td>
-        <td>${customer.address}</td>
-        <td>${customer.city}</td>
-        <td>${customer.postal}</td>
-        <td>${customer.province}</td>
-            <td><button onclick="editCustomer(${i})">Edit</button></td>
-        `;
-        tbody.appendChild(row);
-    }
-}
-
-
-// Function to filter people based on the search input
-function filterCustomer() {
-    const input = document.getElementById("searchInput");
-    const filter = input.value.toLowerCase();
-    const table = document.getElementById("customerTable");
-    const tbody = table.getElementsByTagName("tbody")[0];
-    const rows = tbody.getElementsByTagName("tr");
-
-    for (let i = 0; i < rows.length; i++) {
-        const row = rows[i];
-        const columns = row.getElementsByTagName("td");
-        let found = false;
-
-        for (let j = 0; j < columns.length; j++) {
-            const cell = columns[j];
-            if (cell) {
-                const text = cell.textContent.toLowerCase();
-                if (text.includes(filter)) {
-                    found = true;
-                    break;
-                }
-            }
-        }
-
-        if (found) {
-            row.style.display = "";
-        } else {
-            row.style.display = "none";
-        }
-    }
-}
-
+// Function to edit customer information
 function editCustomer(index) {
-    // Get the equipment data to edit
+    // Get the customer data to edit
     const customerToEdit = hardCodedCustomer[index];
 
-    // Convert the equipment data to a JSON string
+    // Convert the customer data to a JSON string
     const customerDataString = JSON.stringify(customerToEdit);
 
-    // Redirect to inventoryEdit.html with equipment data as a parameter
+    // Redirect to customerEdit.html with customer data as a parameter
     window.location.href = `customerEdit.html?customerData=${customerDataString}`;
 }
 
-function createCustomer(index) {
-    // Get the equipment data to edit
-    const customerToCreate = hardCodedCustomer[index];
-
-    // Convert the equipment data to a JSON string
-    const customerDataString = JSON.stringify(customerToCreate);
-
-    // Redirect to inventoryEdit.html with equipment data as a parameter
-    window.location.href = `customerCreate.html?customerData=${customerDataString}`;
-}
-
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("searchInput").addEventListener("input", filterCustomer);
-    fillTableWithHardCodedEquipment();
+    fillTableWithCustomerData();
 });
