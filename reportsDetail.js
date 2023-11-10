69cb42fa8153f2fd0a0cdbfd3e384e5e777ee9b0
DBAPTZ <123421505+DBAPTZ@users.noreply.github.com>
2023-11-10 17:58:36 -0500

    Update reportsDetail.js

--- reportsDetail.js
+++ reportsDetail.js
@@ -8,20 +8,61 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const selectedReport = JSON.parse(selectedReportString);
 
-    // Populate the report details
-    const reportCustoemrElement = document.getElementById("reportCustomer");
-    const reportDateElement = document.getElementById("reportDate");
-    const reportDescriptionElement = document.getElementById("reportDescription");
-    const reportPriceElement = document.getElementById("reportPrice");
-    const reportQuantityElement = document.getElementById("reportQuantity");
-    const reportTotalElement = document.getElementById("reportTotal");
+    // Populate common details (owner)
+    document.getElementById("reportOwner").textContent = selectedReport.owner;
 
-    reportCustoemrElement.textContent = selectedReport.customer;
-    reportDateElement.textContent = selectedReport.date;
-    reportDescriptionElement.textContent = selectedReport.description;
-    reportPriceElement.textContent = selectedReport.price;
-    reportQuantityElement.textContent = selectedReport.quantity;
-    reportTotalElement.textContent = selectedReport.total;
+    // Get the table element
+    const reportDetailsTable = document.getElementById("reportDetailsTable");
+
+    // Clear previous content
+    reportDetailsTable.innerHTML = "";
+
+    // Check if the report is not a warranty
+    if (selectedReport.category !== "Warranty") {
+        // Populate and show common details
+        reportDetailsTable.innerHTML += `
+            <tr>
+                <th>Date</th>
+                <td>${selectedReport.date}</td>
+            </tr>
+            <tr>
+                <th>Description</th>
+                <td>${selectedReport.description}</td>
+            </tr>
+            <tr>
+                <th>Price</th>
+                <td>${selectedReport.price}</td>
+            </tr>
+            <tr>
+                <th>Quantity</th>
+                <td>${selectedReport.quantity}</td>
+            </tr>
+            <tr>
+                <th>Total</th>
+                <td>${selectedReport.total}</td>
+            </tr>
+        `;
+    } else {
+        // Populate and show warranty-specific details
+        reportDetailsTable.innerHTML += `
+            <tr>
+                <th>Warranty Start</th>
+                <td>${selectedReport.warrantyStart}</td>
+            </tr>
+            <tr>
+                <th>Warranty End</th>
+                <td>${selectedReport.warrantyEnd}</td>
+            </tr>
+            <tr>
+                <th>Warranty Details</th>
+                <td>${selectedReport.warrantyDetails}</td>
+            </tr>
+            <tr>
+                <th>Additional Warranty Info</th>
+                <td>${selectedReport.additionalWarrantyInfo}</td>
+            </tr>
+        `;
+    }
 });
 
 function goToReportsPage() {
