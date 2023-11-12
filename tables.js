b6f1aace7c59c213e73dae1f24c6e4e07496ed4c
bk <146651288+mobkhan1@users.noreply.github.com>
2023-11-12 17:49:20 -0500

    Update tables.js

--- tables.js
+++ tables.js
@@ -1,41 +1,28 @@
-// Hardcoded data for the colours and types tables
+// Hardcoded data for the tables
 const coloursData = [
-    { name: 'Red' },
-    { name: 'Green' },
-    { name: 'Blue' },
-    { name: 'Yellow' },
-    { name: 'Purple' },
-    { name: 'Pink' },
-    { name: 'Orange' },
-    { name: 'White' },
-    { name: 'Brown' },
-    { name: 'Grey' },
-    { name: 'Black' }
+    { name: 'Red' }, { name: 'Green' }, { name: 'Blue' }, 
+    { name: 'Yellow' }, { name: 'Purple' }, { name: 'Pink' },
+    { name: 'Orange' }, { name: 'White' }, { name: 'Brown' },
+    { name: 'Grey' }, { name: 'Black' }
 ];
 
 const citiesData = [
-    { name: 'Niagara Falls' },
-    { name: 'Toronto' },
-    { name: 'Ottawa' },
-    { name: 'Calgary' },
-    { name: 'Winnipeg' }
+    { name: 'Niagara Falls' }, { name: 'Toronto' }, 
+    { name: 'Ottawa' }, { name: 'Calgary' }, { name: 'Winnipeg' }
 ];
 
 const typesData = [
-    { type: 'Electric lawnmower' },
-    { type: 'Gas powered lawnmower' },
-    { type: 'Bagged lawnmower' },
-    { type: 'Push Mower' },
-    { type: 'Scythe' }
+    { type: 'Electric lawnmower' }, { type: 'Gas powered lawnmower' }, 
+    { type: 'Bagged lawnmower' }, { type: 'Push Mower' }, { type: 'Scythe' }
 ];
 
 const partsData = [
-    { name: 'Bolt' },
-    { name: 'Screw' },
-    { name: 'Washer' },
-    { name: 'Nut' },
-    { name: 'Spring' },
+    { name: 'Bolt' }, { name: 'Screw' }, { name: 'Washer' }, 
+    { name: 'Nut' }, { name: 'Spring' }
 ];
+
+// Add stock information to each part (example stock data)
+partsData.forEach(part => part.stock = Math.floor(Math.random() * 100));
 
 function populateTable(tableId, data) {
     const table = document.getElementById(tableId);
@@ -47,17 +34,35 @@ function populateTable(tableId, data) {
         const cell = row.insertCell();
         cell.textContent = item.name || item.type;
 
-        // Add a Controls column for editing
+        // Add a Stock column for parts
+        if (tableId === 'partsTable') {
+            const stockCell = row.insertCell();
+            stockCell.textContent = item.stock;
+        }
+
+        // Add a Controls column for editing and ordering more
         const controlsCell = row.insertCell();
+        
+        // Create Edit button
         const editButton = document.createElement('button');
         editButton.textContent = 'Edit';
         editButton.onclick = function () {
             editItem(row, tableId);
         };
         controlsCell.appendChild(editButton);
+
+        // Create Order More button (only for parts and types tables)
+        if (tableId === 'partsTable' || tableId === 'typesTable') {
+            const orderMoreButton = document.createElement('button');
+            orderMoreButton.textContent = 'Order More';
+            orderMoreButton.onclick = function () {
+                orderMorePartsOrTypes(item);
+            };
+            controlsCell.appendChild(orderMoreButton);
+        }
     });
 }
-// Function to filter the table based on user input
+
 function filterTable() {
     const searchTerm = document.getElementById('searchInput').value.toLowerCase();
     const activeTableId = Array.from(document.getElementsByClassName('table active')).map(table => table.id)[0];
@@ -67,14 +72,11 @@ function filterTable() {
     else if (activeTableId === 'typesTable') { data = typesData; }
     else if (activeTableId === 'citiesTable') { data = citiesData; }
     else if (activeTableId === 'partsTable') { data = partsData; }
-    else { return; }
 
     const filteredData = data.filter(item => (item.name || item.type).toLowerCase().includes(searchTerm));
     populateTable(activeTableId, filteredData);
 }
 
-
-// Function to show a table and deactivate the other tab
 function showTable(tableId) {
     const tabs = document.getElementById('tabs').getElementsByTagName('button');
     for (const tab of tabs) {
@@ -89,7 +91,6 @@ function showTable(tableId) {
     document.getElementById(tableId).classList.add('active');
 }
 
-// Function to handle editing an item and redirect to the appropriate edit page
 function editItem(row, tableId) {
     const rowIndex = row.rowIndex - 1;
     let editPage, rowType, data;
@@ -97,7 +98,6 @@ function editItem(row, tableId) {
     else if (tableId === 'typesTable') { editPage = 'typesEdit.html'; rowType = 'type'; data = typesData; }
     else if (tableId === 'citiesTable') { editPage = 'citiesEdit.html'; rowType = 'city'; data = citiesData; }
     else if (tableId === 'partsTable') { editPage = 'partsEdit.html'; rowType = 'part'; data = partsData; }
-    else { return; }
 
     const rowData = data[rowIndex];
     const jsonData = JSON.stringify(rowData);
@@ -106,34 +106,6 @@ function editItem(row, tableId) {
     window.location.href = `${editPage}?data=${encodeURIComponent(jsonData)}&${paramName}=${encodeURIComponent(typeName)}`;
 }
 
-// Helper function to get data from a selected table
-function getTableData(tableId) {
-    let data;
-    if (tableId === 'coloursTable') { data = coloursData; }
-    else if (tableId === 'typesTable') { data = typesData; }
-    else if (tableId === 'citiesTable') { data = citiesData; }
-    else if (tableId === 'partsTable') { data = partsData; }
-    return data;
-}
-
-// Update the table to include Edit buttons
-function updateTableWithEditButtons() {
-    const tables = document.getElementsByClassName('table');
-    for (const table of tables) {
-        const rows = table.rows;
-        for (let i = 1; i < rows.length; i++) {
-            const editCell = rows[i].insertCell(-1);
-            const editButton = document.createElement('button');
-            editButton.textContent = 'Edit';
-            editButton.onclick = function () {
-                editItem(rows[i], table.id);
-            };
-            editCell.appendChild(editButton);
-        }
-    }
-}
-
-// Function to update the "Create" button text and link
 function updateCreateButton(buttonText, link) {
     const createButton = document.getElementById('createButton');
     createButton.textContent = buttonText;
@@ -142,54 +114,26 @@ function updateCreateButton(buttonText, link) {
     };
 }
 
-
-// Initial setup for the page
 window.onload = function () {
-    updateTableWithEditButtons();  // Add edit buttons to tables
-    populateTable('coloursTable', coloursData); // Populate initial table
+    populateTable('coloursTable', coloursData);
     populateTable('typesTable', typesData);
     populateTable('citiesTable', citiesData);
     populateTable('partsTable', partsData);
-    showTable('coloursTable'); // Show the colours table by default
-    updateCreateButton('Add Colour', 'coloursCreate'); // Set default create button
+    showTable('coloursTable');
+    updateCreateButton('Add Colour', 'coloursCreate');
 
-    // Set up search filter
     const searchInput = document.getElementById('searchInput');
     searchInput.addEventListener('input', filterTable);
 };
 
-// Call the function to add Edit buttons when the page loads
-updateTableWithEditButtons();
-
-// Initial population of the colours and types tables
-populateTable('coloursTable', coloursData);
-populateTable('typesTable', typesData);
-populateTable('citiesTable', citiesData);
-populateTable('partsTable', partsData);
-
-
-// Activate the "colours" tab by default
-showTable('coloursTable');
-
-// Add an event listener to the search input for real-time filtering
-const searchInput = document.getElementById('searchInput');
-searchInput.addEventListener('input', filterTable);
-
-// Call the function to add Edit buttons when the page loads
-updateTableWithEditButtons();
-
-function makeButtonCreateColourByDefault() {
-    const createButton = document.getElementById('createButton');
-    createButton.onclick = function() {
-        window.location.href = 'coloursCreate.html';
-    }
+// Function for handling the order more action
+function orderMoreParts(row, part) {
+    // Redirect to the reportsCreate.html page
+    window.location.href = 'reportsCreate.html';
 }
 
-// Function to update the "Create" button text and link
-function updateCreateButton(buttonText, link) {
-    const createButton = document.getElementById('createButton');
-    createButton.textContent = buttonText;
-    createButton.onclick = function () {
-        window.location.href = link + '.html';
-    };
+// Function for handling the order more action for parts or types
+function orderMorePartsOrTypes(row, item) {
+    // Redirect to the reportsCreate.html page
+    window.location.href = 'reportsCreate.html';
 }
