69867441975c35c66165793a13d564353327ced5
sjohnston37 <145282480+sjohnston37@users.noreply.github.com>
2023-11-12 19:18:34 -0500

    Update repairSession.js

--- repairSession.js
+++ repairSession.js
@@ -1,8 +1,57 @@
+const partsData = [
+    { name: 'Bolt' },
+    { name: 'Screw' },
+    { name: 'Washer' },
+    { name: 'Nut' },
+    { name: 'Spring' },
+];
+
 document.addEventListener("DOMContentLoaded", function () {
     // Get the equipment data from the URL parameter as a JSON string
     const urlParams = new URLSearchParams(window.location.search);
     const repairDataString = urlParams.get("repairData");
 
+    // Populate initial parts dropdown
+    const initialPartsDropdown = document.querySelector(".part-select");
+    partsData.forEach(part => {
+        const option = document.createElement("option");
+        option.value = part.name;
+        option.text = part.name;
+        initialPartsDropdown.appendChild(option);
+    });
+
+    //automatically fill start time
+    var currentDateTime = new Date();
+    var currentDate = currentDateTime.toISOString().split('T')[0];
+    var currentTime = currentDateTime.toTimeString().split(' ')[0].substring(0, 5);
+
+    document.getElementById('startDate').value = currentDate;
+    document.getElementById('startTime').value = currentTime;
+
+    
+    
+    // Retrieve and parse the repair session data
+    let repairData = null;
+    
+    if (repairDataString) {
+        try {
+            repairData = JSON.parse(repairDataString);
+        } catch (error) {
+            console.error("Error parsing repair data:", error);
+        }
+    }
+
+    // Check the repair session status and update the complete button visibility
+    if (repairData && repairData.status === 'Completed') {
+        const completeButton = document.getElementById('completeButton');
+        completeButton.style.display = 'none';
+    }
+    // Add click event listener to the complete button
+    completeButton.addEventListener('click', function() {
+        // Redirect to the repairs.html page
+        window.location.href = 'repairs.html';
+    });
+        
     if (repairDataString) {
         try {
             // Parse the JSON data
@@ -10,8 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // Populate the input fields with the equipment data
             document.getElementById("repairTitle").textContent = repairToEdit.customer + "'s " + repairToEdit.equipment;
-            document.getElementById("equipmentLabel").value = repairToEdit.equipment;
-            document.getElementById("equipmentDescription").value = repairToEdit.description;
+            document.getElementById("description").value = repairToEdit.description;
 
             // Handle the customer dropdown
             const customerDropdown = document.getElementById("customer");
@@ -86,106 +134,28 @@ function resetValidationStyles() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-
-    //automatically fill start time
-    var currentDateTime = new Date();
-    var currentDate = currentDateTime.toISOString().split('T')[0];
-    var currentTime = currentDateTime.toTimeString().split(' ')[0].substring(0, 5);
-
-    document.getElementById('startDate').value = currentDate;
-    document.getElementById('startTime').value = currentTime;
-
-    // Populate parts dropdown on page load
-    const partsDropdown = document.getElementById("part-select-1");
-    const partsData = [
-        { name: 'Bolt' },
-        { name: 'Screw' },
-        { name: 'Washer' },
-        { name: 'Nut' },
-        { name: 'Spring' },
-    ];
-
-    partsData.forEach(part => {
-        const option = document.createElement("option");
-        option.value = part.name;
-        option.text = part.name;
-        partsDropdown.appendChild(option);
-    });
-
-    // Get the repair data from the URL parameter as a JSON string
-    const urlParams = new URLSearchParams(window.location.search);
-    const repairDataString = urlParams.get("selectedRepair");
-
-    // ... rest of the original script ...
-
-});
-
-// Function to add a new part row
 function addPartRow() {
     const partsList = document.getElementById("parts-list");
     const newRow = document.createElement("div");
+    newRow.className = 'part-row';
+
     newRow.innerHTML = `
-        <div class="part-select-container">
-            <select>` +
-                partsData.map(part => `<option value="${part.name}">${part.name}</option>`).join('') +
-            `</select>
-        </div>
-        <div class="quantity-input-container">
-            <input type="number" class="quantity-input" value="1" min="1">
-        </div>
+        <select class="part-select">
+            <option value="">Select Part</option>
+            ${partsData.map(part => `<option value="${part.name}">${part.name}</option>`).join('')}
+        </select>
+        <input type="number" class="quantity-input" value="1" min="1">
+        <button type="button" class="remove-part-button" onclick="removeThisPartRow(this)">-</button>
     `;
+
     partsList.appendChild(newRow);
 }
-//SJ Add part and quantity
-let partIndex = 2;
 
-    function addPartRow() {
-        const partsList = document.getElementById('parts-list');
-        const newRow = document.createElement('div');
-        newRow.classList.add('part-row');
+function removeThisPartRow(button) {
+    const partRow = button.parentElement;
+    partRow.remove();
+}
 
-        // Create containers for the part select and quantity input
-        const partSelectContainer = document.createElement('div');
-        partSelectContainer.classList.add('part-select-container');
-
-        const quantityInputContainer = document.createElement('div');
-        quantityInputContainer.classList.add('quantity-input-container');
-
-        // Create and append the 'Part' label and select element
-        const partLabel = document.createElement('label');
-        partLabel.textContent = 'Part';
-        partLabel.htmlFor = `part-select-${partIndex}`;
-
-        const newSelect = document.createElement('select');
-        newSelect.id = `part-select-${partIndex}`;
-        newSelect.innerHTML = '<option value="">Select Part</option>'; // Add more options dynamically if needed
-
-        partSelectContainer.appendChild(partLabel);
-        partSelectContainer.appendChild(newSelect);
-
-        // Create and append the 'Quantity' label and input element
-        const quantityLabel = document.createElement('label');
-        quantityLabel.textContent = 'Quantity';
-        quantityLabel.htmlFor = `quantity-input-${partIndex}`;
-
-        const newQuantityInput = document.createElement('input');
-        newQuantityInput.type = 'number';
-        newQuantityInput.id = `quantity-input-${partIndex}`;
-        newQuantityInput.className = 'quantity-input';
-        newQuantityInput.value = '1';
-        newQuantityInput.min = '1';
-
-        quantityInputContainer.appendChild(quantityLabel);
-        quantityInputContainer.appendChild(newQuantityInput);
-
-        // Append the containers to the new row
-        newRow.appendChild(partSelectContainer);
-        newRow.appendChild(quantityInputContainer);
-
-        // Append the new row to the parts list in the DOM
-        partsList.appendChild(newRow);
-
-        // Increment the part index for the next part row
-        partIndex++;
-    }
+    
+    
+   
